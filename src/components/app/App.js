1863a8499a656f0e7dc11e925793b97b01f088c2
Dmitry <eralashpvp@gmail.com>
2020-08-09 16:49:10 +0300

    almost perfect router

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
-
+import Header from '../header';
+import {Route} from 'react-router-dom';
+import HomePage from '../pages/home-page';
+import CartPage from '../pages/cart-page';
 
 
 const App = () => {
 
-  return <div>Hey</div>
+  return <div>
+      <Header/>
+        <Route path="/" component={HomePage} exact/>
+        <Route path="/cart" component={CartPage}/>
+  </div>
 }
 
 
