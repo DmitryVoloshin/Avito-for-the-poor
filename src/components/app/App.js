56c4819eea449ad456beb4a1225743b00e9e712c
Dmitry <eralashpvp@gmail.com>
2020-08-09 17:43:32 +0300

    new comp car-list car-list-item

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from '../header';
-import {Route} from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
 import HomePage from '../pages/home-page';
 import CartPage from '../pages/cart-page';
 
@@ -9,8 +9,10 @@ const App = () => {
 
   return <div>
       <Header/>
+      <Switch>
         <Route path="/" component={HomePage} exact/>
         <Route path="/cart" component={CartPage}/>
+      </Switch>
   </div>
 }
 
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
+import './header.css'
+
 const Header = ( ) =>{
     return(
-        <ul>
+        <ul className="header-main_nav">
             <li>
-                <Link to="/">Home</Link>
+                <Link to="/" className="header-nav_item">Home</Link>
             </li>
             <li>
-                <Link to="/cart">Cart</Link>
+                <Link to="/cart" className="header-nav_item">Cart</Link>
             </li>
         </ul>
     )
